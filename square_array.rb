833426b924f8fbd8d74e09255da4274f15785197
Pablo Eusebio <nunieboy88@gmail.com>
2018-09-21 19:14:31 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,15 +1,9 @@
 def square_array(array)
-  # array.each do |y|
-  #   new_number = y ** 2
-  # end
-  # array.each_index do |x|
-  #   array[x] = new_number
-  # end
-  n = 0
-  while array.length > n do
-    array.each do |x|
+  array.each do |x|
+    let n = 0
+    while array.length > n do
       array[n] = x ** 2
-      n = n + 1
+      n += 1
     end
   end
 end
