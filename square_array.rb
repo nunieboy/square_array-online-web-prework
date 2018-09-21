e60bf7e38ea3c0e941bd9f1ebd9c9debee1532f0
Pablo Eusebio <nunieboy88@gmail.com>
2018-09-21 19:27:12 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,12 @@
 def square_array(array)
-  array.each_index do |x|
-    array[x] = x ** 2
+  # array.each_index do |x|
+  #   array[x] = x ** 2
+  # end
+  new_array = Array.new
+  array.each do |x|
+    new_array.push(x ** 2)
   end
+  new_array
 end
 
 
